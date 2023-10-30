a52ccf20913d95351e4dd67d1590ce6c28544b1b
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-30 18:40:50 +0900

    [Add] 섹션 5. 싱글톤 방식의 주의점 - stateless한 설계 (#2)

--- 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singletone/StatefulServiceTest.java
+++ 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singletone/StatefulServiceTest.java
@@ -1,2 +1,33 @@
-package hello.core.singletone;public class StatefulServiceTest {
+package hello.core.singletone;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A 사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+
+        // ThreadB: B 사용자 10000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자 A 주문 금액 조회
+        System.out.println("price = " + userAPrice);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
 }
