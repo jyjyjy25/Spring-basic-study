6e54c3315cdde3b1e9154d00056c4c8ab2640217
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-30 17:34:30 +0900

    [Add] 섹션 5. 싱글톤 패턴 (#2)

--- 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singletone/SingletonService.java
+++ 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singletone/SingletonService.java
@@ -1,2 +1,19 @@
-package hello.core.singletone;public class SingletonService {
+package hello.core.singletone;
+
+public class SingletonService {
+
+    // 1. static 영역에 객체를 딱 1개만 생성한다. (외부에서 접근하지 못하도록 private으로 생성)
+    private static final SingletonService instance = new SingletonService();
+
+    // 2. Singleton 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 public으로 선언한다.
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 3. private 생성자 (외부에서 new로 객체를 생성하는 것을 막기 위함)
+    private SingletonService() {};
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
