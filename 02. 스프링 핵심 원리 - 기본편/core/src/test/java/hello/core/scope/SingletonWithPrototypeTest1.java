35ae90a5dfc8f33f78d2df283e73ae84fa71bef5
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-04 00:44:30 +0900

    [Add] 섹션 9. 프로토타입 스코프 - 싱글톤 빈과 함께 사용 시 문제점 해결 - 스프링 컨테이너에 요청 (#2)

--- 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 02. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -38,20 +39,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // ClientBean 생성 시점에 주입됨(참조값)
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ApplicationContext ac;
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
