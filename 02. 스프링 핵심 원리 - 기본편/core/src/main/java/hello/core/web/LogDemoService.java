9e7a5f579da69aef228e82f713b45d1d87b40bd7
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-11 00:07:41 +0900

    [Add] 섹션 9. 스코프와 Provider (#2)

--- 02. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/web/LogDemoService.java
+++ 02. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // myLogger를 찾을 수 있는 ObjectProvider 주입
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject(); // ObjectProvider로 myLogger 조회(DL)
         myLogger.log("service id = " + id);
     }
 }
